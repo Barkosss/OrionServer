3052d1f353df6136694fde0fd3bd3d2263d75a06
Andrey Baryshev <andrey.bar66@gmail.com>
2026-05-03 04:45:07 +0500

    Init header for Server

---
+++ src/server/Server.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Andrey Baryshev on 03.05.2026.
+//
+
+#include "Server.h"
---
+++ src/server/Server.h
@@ -0,0 +1,18 @@
+//
+// Created by Andrey Baryshev on 03.05.2026.
+//
+
+#ifndef MSLAUNCHER_SERVER_H
+#define MSLAUNCHER_SERVER_H
+
+
+class Server {
+
+public:
+
+    Server(int port);
+    void run();
+};
+
+
+#endif //MSLAUNCHER_SERVER_H
