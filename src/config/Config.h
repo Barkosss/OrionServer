e353ef1d0cf8dbd5a9115b61f72accf8515ea336
Andrey Baryshev <andrey.bar66@gmail.com>
2026-05-03 02:41:13 +0500

    Add methods in Config

--- src/config/Config.h
+++ src/config/Config.h
@@ -5,9 +5,20 @@
 #ifndef MSLAUNCHER_CONFIG_H
 #define MSLAUNCHER_CONFIG_H
 
+#include<string>
+#include<fstream>
 
 class Config {
+    std::string logFilename;
+    std::fstream logFile;
 
+public:
+    Config();
+    ~Config();
+
+    std::string getLogFilename();
+    void setLogFilename(std::string logFilename);
+    std::fstream getLogFile();
 };
 
 
