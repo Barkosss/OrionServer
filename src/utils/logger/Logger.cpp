9ab68ab9c6acec00bab8f5a7d06f6833983188f7
Barkosss <andrey.bar66@gmail.com>
2026-05-02 21:46:50 +0500

    Update Logger

--- src/utils/logger/Logger.cpp
+++ src/utils/logger/Logger.cpp
@@ -3,3 +3,16 @@
 //
 
 #include "Logger.h"
+
+const std::map<LogLevel, std::string> levelToString {
+    {LogLevel::INFO, "INFO"},
+        {LogLevel::WARNING, "WARNING"},
+        {LogLevel::ERROR, "ERROR"},
+        {LogLevel::DEBUG, "DEBUG"},
+};
+
+LogLevel Logger::logLevel = LogLevel::INFO;
+
+void Logger::log(const std::string &logLevel, const std::string message) {
+
+}
--- src/utils/logger/Logger.h
+++ src/utils/logger/Logger.h
@@ -1,9 +1,13 @@
 //
 // Created by Andrey Baryshev on 02.05.2026.
 //
+#pragma once
 
 #ifndef MSLAUNCHER_LOGGER_H
 #define MSLAUNCHER_LOGGER_H
+
+#include<string>
+#include<map>
 
 enum class LogLevel {
     INFO,
@@ -12,10 +16,26 @@ enum class LogLevel {
     DEBUG
 };
 
+extern const std::map<LogLevel, std::string> levelToString;
+
 class Logger {
     static LogLevel logLevel;
 
     static void log(const std::string &logLevel, const std::string message);
+
+public:
+
+    template<typename... Args>
+    static void info(const std::string &format, const Args &...args);
+
+    template <typename... Args>
+    static void warning(const std::string &format, const Args &...args);
+
+    template <typename... Args>
+    static void error(const std::string &format, const Args &...args);
+
+    template <typename... Args>
+    static void debug(const std::string &format, const Args &...args);
 };
 
 
