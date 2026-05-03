1a39d45af0cd69b53cdb2ddde263f0f7a5eb083d
Andrey Baryshev <andrey.bar66@gmail.com>
2026-05-04 01:28:17 +0500

    Add routers

--- src/main.cpp
+++ src/main.cpp
@@ -5,13 +5,20 @@
 #include<iostream>
 #include <exception>
 
+#include "server/Router.h"
 #include "server/Server.h"
 
 int main() {
 
     try {
+        Router router;
 
-        Server server(8080);
+        router.addRoute("/manifest", nullptr);
+        router.addRoute("/file", nullptr);
+        router.addRoute("filelist", nullptr);
+        router.addRoute("health", nullptr);
+
+        Server server(router, 8080);
         server.run();
 
     } catch (std::exception &ex) {
