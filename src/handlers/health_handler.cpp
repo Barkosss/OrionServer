26f89d361bf3eba73345753b0a8d4b7f38d0b0f9
Andrey Baryshev <andrey.bar66@gmail.com>
2026-05-05 03:21:12 +0500

    Complete small endpoint for health

--- src/handlers/health_handler.cpp
+++ src/handlers/health_handler.cpp
@@ -2,7 +2,13 @@
 // Created by Andrey Baryshev on 04.05.2026.
 //
 #include "manifest_handler.h"
+#include<nlohmann/json.hpp>
+#include "../utils/logger/Logger.h"
 
 void handleHealth(const HttpRequest& req, HttpResponse& res) {
-    // ...
+    try {
+        res.result(http::status::ok);
+    } catch (std::exception& ex) {
+        Logger::error("Failed to send response: {}", ex.what());
+    }
 }
