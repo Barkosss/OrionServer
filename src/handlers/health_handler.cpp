c0dff78aa8db0c1e4cf4a4f61cc9feef3675eaf0
Andrey Baryshev <andrey.bar66@gmail.com>
2026-05-05 02:01:50 +0500

    Remove include

--- src/handlers/health_handler.cpp
+++ src/handlers/health_handler.cpp
@@ -2,7 +2,6 @@
 // Created by Andrey Baryshev on 04.05.2026.
 //
 #include "manifest_handler.h"
-//#include<nlohman/json.hpp>
 
 void handleHealth(const HttpRequest& req, HttpResponse& res) {
     // ...
