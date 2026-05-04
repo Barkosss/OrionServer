e47c49a00199ed9f3344d71b9f8beb44e7251862
Andrey Baryshev <andrey.bar66@gmail.com>
2026-05-05 03:22:47 +0500

    Small update file handler

--- src/handlers/file_handler.cpp
+++ src/handlers/file_handler.cpp
@@ -2,8 +2,26 @@
 // Created by Andrey Baryshev on 04.05.2026.
 //
 #include "manifest_handler.h"
-//#include<nlohman/json.hpp>
+#include<nlohmann/json.hpp>
+#include<vector>
+#include "../utils/logger/Logger.h"
+
+void getFileList(nlohmann::json reqFiles, std::vector<std::string> *files);
 
 void handleFile(const HttpRequest& req, HttpResponse& res) {
-    // ...
+    try {
+        nlohmann::json reqFiles(req.body());
+        std::vector<std::string> files;
+        getFileList(reqFiles, files);
+
+        // ...
+
+    } catch (std::exception& ex) {
+        Logger::error("Failed to send response: ", ex.what());
+    }
 }
+
+void getFileList(nlohmann::json reqFiles, std::vector<std::string> *files) {
+    // TODO: Get list of file for send
+}
+
