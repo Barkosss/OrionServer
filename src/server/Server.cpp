e686c923d5690b3e213806a69f1a2c0a0a1c2af3
Andrey Baryshev <andrey.bar66@gmail.com>
2026-05-05 02:02:57 +0500

    Update Server to get/send data

--- src/server/Server.cpp
+++ src/server/Server.cpp
@@ -2,15 +2,15 @@
 // Created by Andrey Baryshev on 03.05.2026.
 //
 
+#include<iostream>
 #include "Server.h"
 #include "../utils/logger/Logger.h"
 #include <boost/asio.hpp>
+#include <boost/beast.hpp>
 
 using boost::asio::ip::tcp;
 
-Server::Server(Router router, unsigned short port) : port(port) {
-    this->router = router;
-    this->port = port;
+Server::Server(Router router, unsigned short port) : router(std::move(router)), port(port) {
     Logger::info("Server object created on port ", port);
 }
 
@@ -31,9 +31,40 @@ void Server::run() {
 
             Logger::info("Client connected from ", socket.remote_endpoint().address().to_string());
 
-            std::string message = "Hello from MSLauncher!\n";
-            boost::system::error_code ignored_error;
-            boost::asio::write(socket, boost::asio::buffer(message), ignored_error);
+            beast::flat_buffer buffer;
+            http::request<http::string_body> req;
+            beast::error_code ec;
+
+            http::read(socket, buffer, req, ec);
+
+            if (ec) {
+                Logger::error("Failed to read request: ", ec.message());
+                continue;
+            }        
+
+            std::string path = req.target();
+            Logger::info("Request: ", req.method_string());
+
+            auto handler = router.getHandler(path);
+
+            http::response<http::string_body> res;
+
+            if (handler) {
+                handler(req, res);
+            } else { // 404
+                res.result(http::status::not_found);
+                res.body() = "404 Not Found";
+                res.set(http::field::content_type, "text/plain");
+            }
+
+            // res.set(http::field::content_length, res.body().size());
+            http::write(socket, res, ec);
+
+            if (ec) {
+                Logger::error("Failed to send response: ", ec.message());
+            }
+
+            socket.shutdown(tcp::socket::shutdown_send, ec);
         }
 
     } catch (const std::exception& ex) {
