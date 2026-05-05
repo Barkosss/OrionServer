b9056cbfcdfdc9782267ddcd2755b107ae51e456
Barkosss <andrey.bar66@gmail.com>
2026-05-05 21:07:46 +0500

    Get file to send & Send

--- src/handlers/file_handler.cpp
+++ src/handlers/file_handler.cpp
@@ -2,25 +2,63 @@
 // Created by Andrey Baryshev on 04.05.2026.
 //
 #include "manifest_handler.h"
+#include<map>
 #include<nlohmann/json.hpp>
 #include<vector>
+#include<fstream>
+#include "../config/Config.h"
 #include "../utils/logger/Logger.h"
 
-void getFileList(nlohmann::json reqFiles, std::vector<std::string> &files);
+using std::map;
+using std::string;
+using std::vector;
+
+bool _getFileStream(vector<string> reqFiles, map<string, std::fstream> &files);
 
 void handleFile(const HttpRequest& req, HttpResponse& res) {
     try {
-        nlohmann::json reqFiles(req.body());
-        std::vector<std::string> files;
-        getFileList(reqFiles, files);
+        nlohmann::json jsonBody(req.body());
+        vector<string> reqFiles;
+        if (jsonBody.contains("files") && jsonBody["files"].is_array()) {
+            reqFiles = vector<string>(jsonBody["files"]);
+        } else {
+            res.body() = nlohmann::json({"error", "Request require list of filenames"}).dump();
+            res.result(http::status::bad_request);
+            return;
+        }
 
-        // ...
+        map<string, std::fstream> files;
+        bool isAll = _getFileStream(reqFiles, files);
+        nlohmann::json fileBody;
+        for (string filename : reqFiles) {
+            if (files.find(filename) == files.end()) {
+                continue;
+            }
+
+            std::string data((std::istreambuf_iterator<char>(files[filename])), std::istreambuf_iterator<char>());
+            fileBody[filename] = data;
+        }
+        
+        res.body() = fileBody.dump();
+        res.set(http::field::content_type, "application/octet-stream");
+        res.result(((isAll) ? (http::status::ok) : (http::status::partial_content)));
 
     } catch (std::exception& ex) {
         Logger::error("Failed to send response: ", ex.what());
     }
 }
 
-void getFileList(nlohmann::json reqFiles, std::vector<std::string> &files) {
-    // TODO: Get list of file for send
+bool _getFileStream(vector<string> reqFiles, map<string, std::fstream> &files) {
+    string modsDir = Config::getInstance().getModsDir();
+    bool fIsAll = true;
+
+    for (string filename : reqFiles) {
+        if (Config::getInstance().shouldSendToClient(filename)) {
+            files[filename] = std::fstream(modsDir + filename);
+        } else {
+            fIsAll = false;
+        }
+    }
+
+    return fIsAll;
 }
